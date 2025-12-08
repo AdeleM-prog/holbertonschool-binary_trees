623284fb5cccfcb093d464f5c5acc3d0821589fc
AdeleM-prog <12043@holbertonstudents.com>
2025-12-08 20:38:16 +0000

    creation 16-binary_tree_is_perfect.c (a completer)

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,12 @@
+#include "binary_tree.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+  if (tree == NULL)
+    return (0);
+    
+}
